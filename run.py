9f9632806e2e5557fb64e65d8c6e9e40aeb71418
LySeriuos <drungilas.p@gmail.com>
2021-10-16 13:59:23 +0000

    template preparation

---
+++ run.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", page_title="About")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html", page_title="Contact")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
+
+
+# this is only if on test. It shouldn't be on normal basis
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)  
