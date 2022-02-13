112bae21dfdaa7a89fc300850bc1e1639f38767f
LySeriuos <drungilas.p@gmail.com>
2022-02-13 21:00:08 +0000

    changed app first.py debug settings

--- first.py
+++ first.py
@@ -77,7 +77,7 @@ def login():
 
 if __name__ == "__main__":
     app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
         debug=True)
         
