c936dc91de5ad53e1fab58e42dc7102fd2a9c7c7
LySeriuos <drungilas.p@gmail.com>
2022-03-07 22:09:00 +0000

    changed database and it's path form JSON file to database

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
+from user import routes
 import os
 import json
 from flask import (
@@ -21,9 +22,6 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
-from user import routes
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -36,21 +34,17 @@ def belenkas():
 
 @app.route("/reviews")
 def reviews():
-    data = []
-    with open("data/company.json", "r") as json_data:
-        data = json.load(json_data)
-    return render_template(
-        "reviews.html", page_title="Hello and Welcome to The Review", company=data)
+    books = mongo.db.books.find()
+    return render_template("reviews.html",
+    page_title="Hello and Welcome to The Review", books=books)
 
 
-@app.route("/reviews/<book_name>")
+@app.route("/reviews/<book_name>", methods=["GET", "POST"])
 def reviews_book(book_name):
     book = {}
-    with open("data/company.json", "r") as json_data:
-        data = json.load(json_data)
-        for obj in data:
-            if obj["url"] == book_name:
-                book = obj                
+    book_name = mongo.db.books.find_one()
+    if book_name["url"] == book_name:
+        book = book_name
     return render_template("book.html", book=book)
 
 
@@ -65,18 +59,18 @@ def about():
 def login():
     if request.method == 'POST':
         # check if user existinfg in the db
-        existing_user = mongo.db.users.find_one(
+        existing_user = mongo.db.members.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username")))
-                    return redirect(url_for(
-                        "member", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username")))
+                return redirect(url_for(
+                    "member", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
@@ -94,7 +88,7 @@ def login():
 def register():
     if request.method == "POST":
         # to check if username exists in db
-        existing_user = mongo.db.userrs.find_one(
+        existing_user = mongo.db.members.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
@@ -102,9 +96,9 @@ def register():
             return redirect(url_for("login"))
 
         register = {"username": request.form.get("username").lower(),
-        "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
+                    "password": generate_password_hash(request.form.get("password"))
+                    }
+        mongo.db.members.insert_one(register)
 
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
@@ -117,11 +111,11 @@ def register():
 @app.route("/member/<username>", methods=["GET", "POST"])
 def member(username):
     # get username from database
-    username = mongo.db.users.find_one(
+    username = mongo.db.members.find_one(
         {"username": session["user"]})["username"]
     if session["user"]:
-        tasks = list(mongo.db.tasks.find())
-        return render_template("member.html", username=username, tasks=tasks)
+        user_reviews = list(mongo.db.user_reviews.find())
+        return render_template("member.html", username=username, user_reviews=user_reviews)
     return redirect(url_for("login"))
 
 
@@ -136,16 +130,16 @@ def logout():
 @app.route("/add_review", methods=["GET", "POST"])
 def add_review():
     if request.method == "POST":
-        task = {
-            "book_name": {{ book.name }},
-            "author":  {{ book.author }},
+        review = {
+            "book_name": request.form.get("book_name"),
+            "author":  request.form.get("book_author"),
             "rating": request.form.get("rating"),
             "review": request.form.get("review"),
             "date": request.form.get("date"),
             "time": request.form.get("time"),
             "created_by": session["user"]
         }
-        mongo.db.tasks.insert_one(task)
+        mongo.db.user_reviews.insert_one(review)
         flash("Thank you for your review!")
         return redirect(url_for("reviews"))
     categories = mongo.db.reviews.find().sort("review_name")
---
+++ test.py
@@ -0,0 +1,27 @@
+import os
+import json
+from flask import (
+    Flask, render_template, request,
+    flash, url_for, redirect, session)
+from werkzeug.wrappers import Request, Response
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo (app)
+
+
+@app.route("/")
+@app.route("/get_books")
+def get_books():
+    books = mongo.db.books.find()
+    return render_template("reviews.html", books=books)
