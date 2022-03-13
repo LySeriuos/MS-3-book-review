a65fba0d88482b9312290ee911b8f67248b3148a
LySeriuos <drungilas.p@gmail.com>
2022-03-13 14:45:54 +0000

    add star rating to the form in book.html

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,5 +10,22 @@ $(document).ready(function(){
     document.getElementById("currentBookName").value = bookName;
     let bookAuthor = document.getElementById("getAuthor").textContent;
     document.getElementById("currentAuthor").value = bookAuthor;
+
+    const container = document.querySelector('.rating');
+    const items = container.querySelectorAll('.rating-item')
+    container.onclick = e => {
+    const elClass = e.target.classList;
+// change the rating if the user clicks on a different star
+    if (!elClass.contains('active')) {
+    items.forEach( // reset the active class on the star
+    item => item.classList.remove('active')
+    );
+    console.log(e.target.getAttribute("data-rate"));
+    elClass.add('active'); // add active class to the clicked star
+    }
+    };
     
+
+
+  
 });
